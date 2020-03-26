b4ebedb83ca0c841b7ed841824a6f28133dec70b
Mateen Irshad <reviewingcode@gmail.com>
2020-03-26 05:53:48 +0500

    feat: capture front-camera image (ios, android)

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
+    <CaptureImage />
   </div>
 </template>
 
 <script>
+import CaptureImage from './components/CaptureImage.vue';
+
 export default {
   name: 'App',
+
+  components: {
+    CaptureImage,
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100vw;
+  min-height: 100vh;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333333;
+  overflow-y: hidden;
 }
 </style>
---
+++ src/components/CaptureImage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <!-- button -->
+    <button
+      v-if="!imageURL"
+      class="button"
+      @click="$refs['input'].click()"
+    >
+      Take Picture
+    </button>
+
+    <!-- preview -->
+    <img
+      v-if="imageURL"
+      class="image"
+      :src="imageURL"
+    >
+
+    <!-- file input -->
+    <input
+      v-show="false"
+      ref="input"
+      type="file"
+      accept="image/*"
+      capture="user"
+      @input="setImage"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CaptureImage',
+
+  data() {
+    return {
+      imageURL: null,
+    };
+  },
+
+  methods: {
+    setImage(e) {
+      if (!e.target.files[0]) return;
+
+      // eslint-disable-next-line prefer-destructuring
+      this.imageURL = URL.createObjectURL(e.target.files[0]);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button {
+  width: 200px;
+  margin-top: 60px;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: limegreen;
+  border: none;
+  border-radius: 4px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.image {
+  min-width: 50vw;
+  max-width: 100vw;
+  min-height: 50vh;
+  max-height: 100vh;
+}
+</style>
